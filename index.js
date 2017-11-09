7d9b65c68b7afdd7c2192581d21b77a36f920a56
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 18:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 function scuberGreetingForFeet(travelDistance) {
   if (travelDistance <= 400) {
     return 'This one is on me!';
+  } else if (travelDistance >= 2000 && travelDistance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
   }
 }
