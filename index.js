232287a8badeb4299ea8a2ccc5e555a3ee28c577
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 18:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function ternaryCheckCity(destinationCity) {
   return destinationCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip() {
+function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
